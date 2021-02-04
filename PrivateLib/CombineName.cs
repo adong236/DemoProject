9871f1a76dbe6051fd465b67cbb766e7b75176c6
Nelson Yan (CSI Interfusion Inc) <v-jiandy@microsoft.com>
2021-02-04 12:27:02 +0800

    add sourcelink demo code

---
+++ PrivateLib/CombineName.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PrivateLib
+{
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class CombineName
+    {
+        private string currentFirstName = string.Empty;
+        private string currentMiddleName = string.Empty;
+        private string currentLastName = string.Empty;
+
+        CombineName(Name name)
+        {
+            currentFirstName = name.FirstName ?? "";
+            currentMiddleName = name.MiddleName ?? "";
+            currentLastName = name.LastName ?? "";
+        }
+
+        public string GetName()
+        {
+            return string.Concat(currentFirstName, currentMiddleName, currentLastName);
+        }
+    }
+
+}
